1c03ba939262d4a4abf50989a3b318af6d5b5f44
Equa <TheSuspect9980@gmail.com>
2025-01-25 22:49:58 +0300

    Initial Commit

---
+++ MP01/MP01App.swift
@@ -0,0 +1,17 @@
+//
+//  MP01App.swift
+//  MP01
+//
+//  Created by Alex Barauskas on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MP01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MP01Tests/MP01Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MP01Tests.swift
+//  MP01Tests
+//
+//  Created by Alex Barauskas on 25.01.2025.
+//
+
+import Testing
+
+struct MP01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
